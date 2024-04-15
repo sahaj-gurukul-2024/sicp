37e2218c5d681e68995755261c3d891e1cf154ff
Sara Paul <paul.sara@gmail.com>
2024-04-16 03:24:41 +0530

    Implement Tower of Hanoi

--- src/sicp/tower_of_hanoi.clj
+++ src/sicp/tower_of_hanoi.clj
@@ -1,2 +1,9 @@
 (ns sicp.tower-of-hanoi)
 
+(defn move-tower [n from to spare]
+  (if (= n 0) ""
+              (str
+                (move-tower (- n 1) from spare to)
+                (str "Ring " n " " from " " to "\n")
+                (move-tower (- n 1) spare to from))))
+
--- test/sicp/tower_of_hanoi_test.clj
+++ test/sicp/tower_of_hanoi_test.clj
@@ -1,3 +1,7 @@
 (ns sicp.tower-of-hanoi-test
   (:require [clojure.test :refer :all])
   (:require [sicp.tower-of-hanoi :refer :all]))
+
+
+(deftest move-tower-test
+  (println (move-tower 4 "from" "to" "spare")))
